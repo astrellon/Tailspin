089428875f2f73be4e3c19ab9347c102a640d758
Astrellon <sovereign250@gmail.com>
2014-06-13 09:30:49 +1000

    - Helper functions for using flag enums in the editor.

---
+++ Assets/Scripts/Utils/BitMaskAttribute.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEditor;
+
+// Have to be defined somewhere in a runtime script file
+public class BitMaskAttribute : PropertyAttribute
+{
+    public System.Type propType;
+    public BitMaskAttribute(System.Type aType)
+    {
+        propType = aType;
+    }
+}
